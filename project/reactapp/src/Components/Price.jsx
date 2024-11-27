18e351c9f2fa32acd91169bea22fd3717e7eac83
Uchenna Onwuliri <uchennaonwuliri@gmail.com>
2024-11-27 23:47:03 +0100

    navbar realigned, pricing page complete, home page structure complete, next up about page and user profile info

--- project/reactapp/src/Components/Price.jsx
+++ project/reactapp/src/Components/Price.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import priceIcon from '../assets/price icon.png'
+import priceIcon from '../assets/priceIcon.svg'
 
 const Price = ({price,pricingName,discount}) => {
   return (
       <div className="pricing-card text-start">
                     <div className="header">
                         <img src={priceIcon} alt="" srcset="" />
-                        <p className='font-semibold py-2'>{pricingName}</p>
+                        <p className='font-semibold text-[30px] py-2'>{pricingName}</p>
                     </div>
                     <div className="price">
-                    <small className='text-[#5D5A6F]'>₦{discount}/Monthly</small>
-                        <p>₦{price}.00</p>
-                        <button>Subscribe</button>
+                    <p className='text-[#ff9500] '><span className='text-[20px]'>₦</span>{discount}/Monthly</p>
+                        <p className='text-[30px] font-semibold pb-2'><span className='text-[20px]'>₦</span>{price}</p>
+                        <button>Enroll</button>
                     </div>
-                </div>
+      </div>
   )
+  
 }
 
 export default Price
--- project/reactapp/src/pages/Pricing.jsx
+++ project/reactapp/src/pages/Pricing.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import logo from '../assets/pricing.png'
+import logo from '../assets/pricing2.svg'
 import './css/Pricing.css'
 import Price from '../Components/Price'
+import Newsletter from '../Components/Newsletter'
 
 const Pricing = () => {
     const monthly = 9900
   return (
     <div className='px-10 phone:px-20'>
         <div className="herop">
-            <p>Home | <span>Pricing</span></p>
+            <p>Home | <span className='text-[#FF9500]'>Pricing</span></p>
             <div className="flex flex-col tablet:flex-row items-center justify-around pt-5">
-                <h2>Our pre-ready<br/> pricing packages</h2>
+                <h2 className='text-[32px] tablet:text-[45px] font-semibold overflow-hidden'>Our pre-ready<br/> pricing packages</h2>
                 <div>
                     <img src={logo} alt="" srcset="" />
                 </div>
@@ -18,10 +19,10 @@ const Pricing = () => {
         </div>
         <section className='my-10 tablet:my-20 text-center'>
             <div className='pb-10'>
-                <h2 className='pb-5'>
+                <h2 className='pb-5 font-bold normal-case'>
                 We create a monthly pricing package <br/> for all standard students
                 </h2>
-                <div >
+                <div className='pricing-desc' >
                     <p>Basically we create this package for those who are really interested and get benifited from our courses or books. 
                         We want to make a low cost package for them. So that they can purchase any courses with the package they buy from us.
                         Also will get free books from every packages.</p>
@@ -29,29 +30,14 @@ const Pricing = () => {
             </div>
 
             <div className="pricing">
-            <div className="flex flex-col phone:flex-row gap-4 justify-center">
+            <div className="flex flex-col tablet:flex-row gap-4 justify-center">
                 <Price pricingName={'Monthly Subscription'} price={9000} />
                 <Price pricingName={'Yearly Subscription'} price={79900} discount={6658.33}/>
             </div>
         </div>
         </section>
         
-        <section className="newsletter mt-20">
-            <div className=' p-5'>
-                <div className="header pb-5">
-                {/* font-size: 42px;
-                font-weight: 700; */}
-                    <h2 className='text-[25px] phone:text-[38px] tablet:text-[42px] tablet:leading-[55px]'>
-                    Subscribe to Get Update On <br/> Every New Course
-                    </h2>
-                    <small>20k+ students daily learn with Eduvi. Subscribe for new courses.</small>
-                </div>
-                <div className='flex'>
-                    <input className='w-[100px] phone:w-[509px]' type="email" placeholder='enter your email'/>
-                    <button>Subscribe</button>
-                </div>
-            </div>
-        </section>
+       <Newsletter/>
     </div>
   )
 }
